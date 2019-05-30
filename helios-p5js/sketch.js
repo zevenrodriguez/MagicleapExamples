e0803a4fcf75d657c8f11fca1eb85e8d4aeb7ee7
Zevensuy Rodriguez <zevenrodriguez@gmail.com>
2019-05-30 16:45:53 -0400

    clear

--- helios-p5js/sketch.js
+++ helios-p5js/sketch.js
@@ -4,20 +4,25 @@ var posY = 0;
 var xDir = false;
 var yDir = false;
 
+var img;
+
 function setup() {
   // put setup code here
   createCanvas(windowWidth,windowHeight);
   posX = random(width);
   posY = random(height);
 
+  img = loadImage('assets/transbackground.png');
+
 }
 
 function draw() {
   // put drawing code here
-  background(255,255);
-
+  clear();
+  background(255,0);
   fill(255,0,0);
   ellipse(posX,posY,100,100);
+
 
   if(posX > width){
     xDir = true;
@@ -37,15 +42,20 @@ function draw() {
 
   if(xDir == false){
     posX+=5;
+
   }else{
     posX-=5;
+
   }
 
   if(yDir == false){
     posY+=5;
+    image(img,0,0,windowWidth,windowHeight);
+
   }else{
     posY-=5;
+    image(img,0,0,windowWidth,windowHeight);
+
   }
-  background(255,0);
 
 }
