79c76078e092f98cd6ccdb0c238e1356f16ce333
Zevensuy Rodriguez <zevenrodriguez@gmail.com>
2019-05-30 16:33:08 -0400

    bounce

--- helios-p5js/sketch.js
+++ helios-p5js/sketch.js
@@ -1,11 +1,48 @@
+var posX = 0;
+var posY = 0;
+
+var xDir = false;
+var yDir = false;
+
 function setup() {
   // put setup code here
-  createCanvas(400,400);
+  createCanvas(windowWidth,windowHeight);
+  posX = random(width);
+  posY = random(height);
+
 }
 
 function draw() {
   // put drawing code here
   background(255,0);
   fill(255,0,0);
-  ellipse(100,100,100,100);
+  ellipse(posX,posY,100,100);
+
+  if(posX > width){
+    xDir = true;
+  }
+
+  if(posX < 0){
+    xDir = false;
+  }
+
+  if(posY > height){
+    yDir = true;
+  }
+
+  if(posY < 0){
+    yDir = false;
+  }
+
+  if(xDir == false){
+    posX+=5;
+  }else{
+    posX-=5;
+  }
+
+  if(yDir == false){
+    posY+=5;
+  }else{
+    posY-=5;
+  }
 }
